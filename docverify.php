38e38f1feeae709daacb4509d4c14032c823f328
ayush95045 <66023555+ayush95045@users.noreply.github.com>
2020-08-19 14:34:33 +0530

    Up

--- docverify.php
+++ docverify.php
@@ -1,6 +1,6 @@
 <?php 
 $test = "/downverify.html";
-echo "$_POST['url']";
+
 $string = file_get_contents("$_POST['url']"."$test");
 
 preg_match_all("/<title>(.*)<\/title>/i",$string,$title);
@@ -8,5 +8,6 @@ preg_match_all("/<h1>(.*)<\/h1>/i",$string,$h1);
 preg_match_all("/<h2>(.*)<\/h2>/i",$string,$h2);
 
 var_dump($title,$h1,$h2);
+echo "$_POST['url']";
       
 ?>
