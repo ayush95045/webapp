b45713a8df0d833f89f2cc18b60a1490b9a9a578
ayush95045 <66023555+ayush95045@users.noreply.github.com>
2020-08-19 14:20:46 +0530

    Update

--- docverify.php
+++ docverify.php
@@ -1,6 +1,6 @@
 <?php 
 $test = "/downverify.html";
-$string = file_get_contents('https://ayush95045.github.io/webapp/downverify.html');
+$string = file_get_contents($_POST['url'].$test);
 preg_match_all("/<title>(.*)<\/title>/i",$string,$title);
 preg_match_all("/<h1>(.*)<\/h1>/i",$string,$h1);
 preg_match_all("/<h2>(.*)<\/h2>/i",$string,$h2);
