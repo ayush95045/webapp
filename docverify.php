4d612a05026b2fd6ae57e06984e16e0bdfed4b6c
ayush95045 <66023555+ayush95045@users.noreply.github.com>
2020-08-19 14:08:54 +0530

    Update

--- docverify.php
+++ docverify.php
@@ -1,6 +1,6 @@
 <?php 
 $test = "/downverify.html";
-$string = file_get_contents($_POST['url'].$test);
+$string = file_get_contents('https://web-app-ayu.herokuapp.com/docverify.php);
 preg_match_all("/<title>(.*)<\/title>/i",$string,$title);
 preg_match_all("/<h1>(.*)<\/h1>/i",$string,$h1);
 preg_match_all("/<h2>(.*)<\/h2>/i",$string,$h2);
