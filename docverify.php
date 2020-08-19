fb3df05ae6fd185e1ff2a9b3a294f6da63f66db9
ayush95045 <66023555+ayush95045@users.noreply.github.com>
2020-08-19 13:58:42 +0530

    Update

--- docverify.php
+++ docverify.php
@@ -4,8 +4,6 @@ $string = file_get_contents($_POST['url'].$test);
 preg_match_all("/<title>(.*)<\/title>/i",$string,$title);
 preg_match_all("/<h1>(.*)<\/h1>/i",$string,$h1);
 preg_match_all("/<h2>(.*)<\/h2>/i",$string,$h2);
-echo "$title";
-     "$h1";
-     "$h2";
-     
- ?>
+var_dump($title,$h1,$h2);
+      
+?>
