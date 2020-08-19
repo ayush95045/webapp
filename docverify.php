842071e1eb4dc3df01cad2f00fdacdbb7732688b
ayush95045 <66023555+ayush95045@users.noreply.github.com>
2020-08-19 14:35:37 +0530

    Up

--- docverify.php
+++ docverify.php
@@ -7,7 +7,7 @@ preg_match_all("/<title>(.*)<\/title>/i",$string,$title);
 preg_match_all("/<h1>(.*)<\/h1>/i",$string,$h1);
 preg_match_all("/<h2>(.*)<\/h2>/i",$string,$h2);
 
-var_dump($title,$h1,$h2);
-echo "$_POST['url']";
+var_dump($title,$h1,$h3);
+var_dump($_POST['url']);
       
 ?>
