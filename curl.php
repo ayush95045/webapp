978d03aa045a7a8deca2ecf73e82f53a59dad8f8
ayush95045 <66023555+ayush95045@users.noreply.github.com>
2020-09-12 23:17:38 +0530

    Up

--- curl.php
+++ curl.php
@@ -3,7 +3,7 @@ if (isset($_POST['data'])) {
 
 $data = $_POST['data'];
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://localhost:81/curl2.php");
+curl_setopt($ch, CURLOPT_URL, "https://web-app-ayu.herokuapp.com/curl2.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
